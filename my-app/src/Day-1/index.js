c4b8b20c19686e62fcaecc8ff1ac500484b10414
DV.Sang <vansangIT2k3@gmail.com>
2024-10-18 15:34:38 +0700

    done day 1

--- my-app/src/Day-1/index.js
+++ my-app/src/Day-1/index.js
@@ -1,15 +1,46 @@
-function randomNumber(num) {
-    return Math.floor(Math.random() * num);
-}
-
-function shuffleArray(arr) {
-    for (var i = 0; i < arr.length; i++) {
-        var randomIndex = randomNumber(arr.length);
-        let temp = arr[i];
-        arr[i] = arr[randomIndex];
-        arr[randomIndex] = temp;
+class Person {
+    constructor(firstName, lastName, age, country, city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+        this.city = city;
+        this.score = 0;
+        this.skills = [];
     }
-    return arr;
+    getFullName() {
+        const fullName = this.firstName + " " + this.lastName;
+        return fullName;
+    }
+    get getScore() {
+        return this.score;
+    }
+    get getSkills() {
+        return this.skills;
+    }
+    set setScore(score) {
+        this.score += score;
+    }
+    set setSkill(skill) {
+        this.skills.push(skill);
+    }
 }
 
-console.log(shuffleArray([1, 2, 3, 4, 5]));
+const person1 = new Person("Asabeneh", "Yetayeh", 250, "Finland", "Helsinki");
+const person2 = new Person("Lidiya", "Tekle", 28, "Finland", "Espoo");
+
+person1.setScore = 1;
+person1.setSkill = "HTML";
+person1.setSkill = "CSS";
+person1.setSkill = "JavaScript";
+
+person2.setScore = 1;
+person2.setSkill = "Planning";
+person2.setSkill = "Managing";
+person2.setSkill = "Organizing";
+
+console.log(person1.score);
+console.log(person2.score);
+
+console.log(person1.skills);
+console.log(person2.skills);
