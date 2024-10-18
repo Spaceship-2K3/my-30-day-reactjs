52be20701160a6bab77dc7909125a6f07696d7a3
DV.Sang <vansangIT2k3@gmail.com>
2024-10-18 14:10:48 +0700

    countinue day 1

--- my-app/src/Day-1/index.js
+++ my-app/src/Day-1/index.js
@@ -1,1 +1,15 @@
- 
+function randomNumber(num) {
+    return Math.floor(Math.random() * num);
+}
+
+function shuffleArray(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var randomIndex = randomNumber(arr.length);
+        let temp = arr[i];
+        arr[i] = arr[randomIndex];
+        arr[randomIndex] = temp;
+    }
+    return arr;
+}
+
+console.log(shuffleArray([1, 2, 3, 4, 5]));
