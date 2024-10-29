0675cf67a533179fb552631687dbbb61a82057df
DV.Sang <vansangIT2k3@gmail.com>
2024-10-29 08:57:08 +0700

    done day 10

--- my-app/src/Day-10/App.js
+++ my-app/src/Day-10/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            src App.js index.js components -auth -Signup.js -Signin.js
+            -Forgotpassword.js -Resetpassord.js header -Header.js footer
+            -Footer.js assets -images -icnons - fonts styles -button.js
+            -button.scss utils -random-id.js -display-time.js -generate-color.js
+            shared -Button.js -InputField.js -TextAreaField.js
+        </div>
+    );
 };
 
 export default App;
